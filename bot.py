da230c205d8dc4f61ea3d43d45551e8482f969dd
Vladka GG <vladlenborgardt@gmail.com>
2020-02-29 11:01:06 +0300

    run_async and beginning chat`s functions

--- bot.py
+++ bot.py
@@ -1,23 +1,17 @@
-from telegram import InlineQueryResultArticle, InputTextMessageContent, Update
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, InlineQueryHandler, CallbackContext
+from telegram import InlineQueryResultArticle, InputTextMessageContent, Update, InlineKeyboardButton, InlineKeyboardMarkup
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, InlineQueryHandler, CallbackContext, CallbackQueryHandler
+from telegram.ext.dispatcher import run_async
 import json
 
-# import logging
 
-# logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname) - %(message)s', level=logging.INFO)
-
-
-updater = Updater(token='829440405:AAFzQJ13Kc3d04TeG4dTyd672M4diFsiR7c', use_context=True)
-dispatcher = updater.dispatcher
-job = updater.job_queue
-
-
+@run_async
 def start(update: Update, context: CallbackContext):
     user = update.effective_user
     context.bot.send_message(chat_id=update.effective_chat.id,
                              text="Hello from bot, {}! Type '/help' to get information about me".format(user.username))
 
 
+@run_async
 def help_callback(update: Update, context: CallbackContext):
     actual_functions = {'/start': 'Just beginning a dialog with me',
                         '/add_group': 'Create new group, tell me name',
@@ -29,7 +23,7 @@ def help_callback(update: Update, context: CallbackContext):
                         '/update_group': 'Update name of some group',
                         '/show_groups': 'Show all groups'}
 
-    text = """It is actual list of my possibilities:\n"""
+    text = """It is actual list of possibilities:\n"""
     for name, description in actual_functions.items():
         text += "{} - {}\n".format(name, description)
     context.bot.send_message(chat_id=update.effective_chat.id, text=text)
@@ -58,6 +52,7 @@ def add_group(update: Update, context: CallbackContext):
     context.bot.send_message(chat_id=update.effective_chat.id, text="I've done!")
 
 
+@run_async
 def show_groups(update: Update, context: CallbackContext):
     with open('groups.json', 'r') as file:
         all_groups = json.loads(file.read())
@@ -87,36 +82,38 @@ def delete_groups(update: Update, context: CallbackContext):
     context.bot.send_message(chat_id=update.effective_chat.id, text="I've done!")
 
 
+@run_async
+def add_chat(update: Update, context: CallbackContext):
+    chat_name = ' '.join(context.args)
+    if not chat_name:
+        context.bot.send_message(chat_id=update.effective_chat.id, text='You should write a chat name after command')
+        return
+    if ' ' in chat_name:
+        context.bot.send_message(chat_id=update.effective_chat.id,
+                                 text='You should write a chat name without " ". Please use "_"')
+        return
+    with open('groups.json', 'r') as file:
+        all_groups = json.loads(file.read())
+    keyboard = list()
+    for group_name in list(all_groups.keys()):
+        print(group_name)
+        keyboard.append([InlineKeyboardButton(group_name, callback_data='|||'.join([group_name, chat_name]))])
+    reply_markup = InlineKeyboardMarkup(keyboard)
+    update.message.reply_text('Please choose a group:', reply_markup=reply_markup)
+
+
+def add_chat_button(update: Update, context: CallbackContext):
+    query = update.callback_query
+    data = query.data.split('|||')
+    chosen_group, chat_name = data[0], data[1]
+    print(chosen_group, chat_name)
+    context.bot.send_message(chat_id=update.effective_chat.id, text='ha loh i niche ne delaiu')
+
+
+@run_async
 def error(update: Update, context: CallbackContext):
     text = "I don't know what to do with this: {}".format(update.message.text)
     context.bot.send_message(chat_id=update.effective_chat.id, text=text)
-
-
-# def caps(update: Update, context: CallbackContext):
-#     text_caps = ' '.join(context.args).upper()
-#     context.bot.send_message(chat_id=update.effective_chat.id, text=text_caps)
-#
-#
-# def inline_caps(update: Update, context: CallbackContext):
-#     query = update.inline_query.query
-#     if not query:
-#         return
-#     results = list()
-#     results.append(
-#         InlineQueryResultArticle(
-#             id=update.inline_query.id,
-#             title='Caps',
-#             input_message_content=InputTextMessageContent(query.upper())))
-#     context.bot.answer_inline_query(update.inline_query.id, results)
-#
-#
-# def timer(context: CallbackContext):
-#     context.bot.send_message(chat_id=context.job.context, text='ALARM MATHAFACKA!!!@!!1')
-#
-#
-# def message_timer(update: Update, context: CallbackContext):
-#     context.bot.send_message(chat_id=update.message.chat_id, text='____Setting a timer for 1 minute____')
-#     context.job_queue.run_once(timer, 60, context=update.message.chat_id)
 
 
 start_handler = CommandHandler('start', start)
@@ -124,19 +121,24 @@ help_handler = CommandHandler('help', help_callback)
 add_group_handler = CommandHandler('add_group', add_group)
 show_groups_handler = CommandHandler('show_groups', show_groups)
 delete_groups_handler = CommandHandler('delete_group', delete_groups)
-# caps_handler = CommandHandler('caps', caps)
-# timer_handler = CommandHandler('timer', message_timer)
-# inline_caps_handler = InlineQueryHandler(inline_caps)
+add_chat_handler = CommandHandler('add_chat', add_chat)
+add_chat_button_handler = CallbackQueryHandler(add_chat_button)
 error_handler = MessageHandler(Filters.text, error)
 
-dispatcher.add_handler(start_handler)
-dispatcher.add_handler(help_handler)
-dispatcher.add_handler(add_group_handler)
-dispatcher.add_handler(show_groups_handler)
-dispatcher.add_handler(delete_groups_handler)
-# dispatcher.add_handler(caps_handler)
-# dispatcher.add_handler(timer_handler)
-# dispatcher.add_handler(inline_caps_handler)
-dispatcher.add_handler(error_handler)
 
-updater.start_polling()
+if __name__ == '__main__':
+    updater = Updater(token='829440405:AAFzQJ13Kc3d04TeG4dTyd672M4diFsiR7c', use_context=True, workers=20)
+    dispatcher = updater.dispatcher
+    job = updater.job_queue
+
+    dispatcher.add_handler(start_handler)
+    dispatcher.add_handler(help_handler)
+    dispatcher.add_handler(add_group_handler)
+    dispatcher.add_handler(show_groups_handler)
+    dispatcher.add_handler(delete_groups_handler)
+    dispatcher.add_handler(add_chat_handler)
+    dispatcher.add_handler(add_chat_button_handler)
+    dispatcher.add_handler(error_handler)
+
+    updater.start_polling()
+    updater.idle()
