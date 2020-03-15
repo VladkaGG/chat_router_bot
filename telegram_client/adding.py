819ec092285e9f2c1696dfc89b1ea87e566193f2
Vladka GG <vladlenborgardt@gmail.com>
2020-03-15 20:19:12 +0300

    some mistakes

--- telegram_client/adding.py
+++ telegram_client/adding.py
@@ -1,11 +1,10 @@
 from telethon.sync import TelegramClient
 from telethon.sessions import StringSession
 from telethon.errors.rpcerrorlist import UserIdInvalidError, ChatIdInvalidError
-from telethon.tl.functions.messages import GetDialogsRequest, AddChatUserRequest
-from telethon.tl.types import InputPeerEmpty, InputUser
+from telethon.tl.functions.messages import AddChatUserRequest
+from telethon.tl.types import InputUser
 from telethon.errors.rpcerrorlist import PeerFloodError, UserPrivacyRestrictedError
 import configparser
-import datetime
 import os
 import logging
 import asyncio
@@ -34,32 +33,21 @@ def add_user(target_chat, user_to_add):
     if not client.is_user_authorized():
         client.send_code_request(phone_number)
         client.sign_in(phone_number, input('Enter the code: '))
-    result = client(GetDialogsRequest(
-        offset_date=datetime.datetime.now(),
-        offset_id=0,
-        offset_peer=InputPeerEmpty(),
-        limit=100,
-        hash=0
-    ))
-    all_chats = []
-    all_chats.extend(result.chats)
-    del result
-    chat_to_add = None
-    for chat in all_chats:
-        if chat.title == target_chat:
-            chat_to_add = client.get_input_entity(chat.title)
-            del all_chats
-            break
+    chat_to_add = ''
+    for dialog in client.iter_dialogs():
+        if dialog.name == target_chat:
+            chat_to_add = dialog.id
     if not chat_to_add:
         error_str = "Some troubles with chat {} " \
                     "(probably you doesn't have admin access or write a wrong chat name)".format(target_chat)
         logger.error(error_str)
+        client.disconnect()
         loop.close()
         return error_str
     error_str = None
     try:
         user = client.get_entity(user_to_add)
-        await client(AddChatUserRequest(chat_to_add, InputUser(user.id, user.access_hash), fwd_limit=10))
+        client(AddChatUserRequest(chat_to_add, InputUser(user.id, user.access_hash), fwd_limit=10))
     except PeerFloodError:
         error_str = "Getting Flood Error from telegram. Script is stopping now. Please try again after some time."
     except UserPrivacyRestrictedError:
@@ -72,5 +60,6 @@ def add_user(target_chat, user_to_add):
     except:
         error_str = "Unexpected"
     finally:
+        client.disconnect()
         loop.close()
         return error_str or "{} added to {}".format(user_to_add, target_chat)
