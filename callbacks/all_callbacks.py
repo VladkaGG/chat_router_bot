a77c08172e41275247645c5366677a39020b5d7b
Vladka GG <vladlenborgardt@gmail.com>
2020-03-15 22:00:00 +0300

    really MVP nowadays

--- callbacks/all_callbacks.py
+++ callbacks/all_callbacks.py
@@ -1,4 +1,4 @@
-from callbacks import run_async, Update, CallbackContext, json, InlineKeyboardButton, InlineKeyboardMarkup, os
+from callbacks import run_async, Update, CallbackContext, json
 from callbacks.Buttons import Markup
 from telegram_client.adding import add_user
 
@@ -49,191 +49,203 @@ def add_group(update: Update, context: CallbackContext):
     context.bot.send_message(chat_id=update.effective_chat.id, text="Done!")
     return 1
 
+
 class DeleteGroup:
-    @run_async
-    def delete_group(update: Update, context: CallbackContext):
-        with open('groups.json', 'r') as file:
-            all_groups = json.loads(file.read())
-        reply_markup = Markup(all_groups)
-        update.message.reply_text('Which group do you want to delete?', reply_markup=reply_markup.return_keyboard())
-        return 1
+    all_groups: dict = {}
 
-    def delete_group_button(update: Update, context: CallbackContext):
-        query = update.callback_query
-        data = query.data
-        choice = data.split('|||')[0]
-        if data == 'exit':
-            context.bot.delete_message(chat_id=query.message.chat_id,
-                                       message_id=query.message.message_id)
-            return 2
-        elif choice in ['prev', 'next']:
-            page = int(data.split('|||')[1])
-            with open('groups.json', 'r') as file:
-                all_groups = json.loads(file.read())
-            reply_markup = Markup(all_groups, page)
-            reply_markup.next() if choice == 'next' else reply_markup.prev()
-            context.bot.edit_message_text(text="Which group do you want to delete?",
-                                          chat_id=query.message.chat_id,
-                                          message_id=query.message.message_id,
-                                          reply_markup=reply_markup.return_keyboard())
-            return 1
 
-        with open('groups.json', 'r') as file:
-            all_groups = json.loads(file.read())
-        all_groups.pop(data)
-        with open('groups.json', 'w') as file:
-            file.write(json.dumps(all_groups))
+@run_async
+def delete_group(update: Update, context: CallbackContext):
+    with open('groups.json', 'r') as file:
+        all_groups = json.loads(file.read())
+        DeleteGroup.all_groups = all_groups
+    reply_markup = Markup(all_groups)
+    update.message.reply_text('Which group do you want to delete?', reply_markup=reply_markup.return_keyboard())
+    return 1
+
+
+def delete_group_button(update: Update, context: CallbackContext):
+    query = update.callback_query
+    data = query.data
+    choice = data.split('|||')[0]
+    if data == 'exit':
         context.bot.delete_message(chat_id=query.message.chat_id,
                                    message_id=query.message.message_id)
-        context.bot.send_message(chat_id=update.effective_chat.id, text="Done!")
         return 2
+    elif choice in ['prev', 'next']:
+        page = int(data.split('|||')[1])
+        reply_markup = Markup(DeleteGroup.all_groups, page)
+        reply_markup.next() if choice == 'next' else reply_markup.prev()
+        context.bot.edit_message_text(text="Which group do you want to delete?",
+                                      chat_id=query.message.chat_id,
+                                      message_id=query.message.message_id,
+                                      reply_markup=reply_markup.return_keyboard())
+        return 1
+    DeleteGroup.all_groups.pop(data)
+    with open('groups.json', 'w') as file:
+        file.write(json.dumps(DeleteGroup.all_groups))
+    DeleteGroup.all_groups = {}
+    context.bot.delete_message(chat_id=query.message.chat_id,
+                               message_id=query.message.message_id)
+    context.bot.send_message(chat_id=update.effective_chat.id, text="Done!")
+    return 2
+
 
 class AddChat:
+    all_groups: dict = {}
     choosing_group_for_adding = ''
 
-    @run_async
-    def add_chat(update: Update, context: CallbackContext):
-        with open('groups.json', 'r') as file:
-            all_groups = json.loads(file.read())
 
-        reply_markup = Markup(all_groups)
-        update.message.reply_text('Please choose a group:', reply_markup=reply_markup.return_keyboard())
-        return 1
+@run_async
+def add_chat(update: Update, context: CallbackContext):
+    with open('groups.json', 'r') as file:
+        all_groups = json.loads(file.read())
+        AddChat.all_groups = all_groups
 
-    @run_async
-    def add_chat_button(update: Update, context: CallbackContext):
-        query = update.callback_query
-        data = query.data
-        choice = data.split('|||')[0]
-        if data == 'exit':
-            context.bot.delete_message(chat_id=query.message.chat_id,
-                                       message_id=query.message.message_id)
-            return 3
-        elif choice in ['prev', 'next']:
-            page = int(data.split('|||')[1])
-            with open('groups.json', 'r') as file:
-                all_groups = json.loads(file.read())
-            reply_markup = Markup(all_groups, page)
-            reply_markup.next() if choice == 'next' else reply_markup.prev()
-            context.bot.edit_message_text(text="Please choose a group:",
-                                          chat_id=query.message.chat_id,
-                                          message_id=query.message.message_id,
-                                          reply_markup=reply_markup.return_keyboard())
-            return 1
-        AddChat.choosing_group_for_adding = data
-        context.bot.edit_message_text(chat_id=query.message.chat_id,
+    reply_markup = Markup(all_groups)
+    update.message.reply_text('Please choose a group:', reply_markup=reply_markup.return_keyboard())
+    return 1
+
+
+@run_async
+def add_chat_button(update: Update, context: CallbackContext):
+    query = update.callback_query
+    data = query.data
+    choice = data.split('|||')[0]
+    if data == 'exit':
+        context.bot.delete_message(chat_id=query.message.chat_id,
+                                   message_id=query.message.message_id)
+        return 3
+    elif choice in ['prev', 'next']:
+        page = int(data.split('|||')[1])
+        reply_markup = Markup(AddChat.all_groups, page)
+        reply_markup.next() if choice == 'next' else reply_markup.prev()
+        context.bot.edit_message_text(text="Please choose a group:",
+                                      chat_id=query.message.chat_id,
                                       message_id=query.message.message_id,
-                                      text='You choose {}\nWrite a name:'.format(data))
-        return 2
+                                      reply_markup=reply_markup.return_keyboard())
+        return 1
+    AddChat.choosing_group_for_adding = data
+    AddChat.all_groups = {}
+    context.bot.edit_message_text(chat_id=query.message.chat_id,
+                                  message_id=query.message.message_id,
+                                  text='You choose {}\nWrite a name:'.format(data))
+    return 2
 
-    def ending_add_chat(update: Update, context: CallbackContext):
-        chat_name = update.message.text
-        chosen_group = AddChat.choosing_group_for_adding
-        with open('groups.json', 'r') as file:
-            all_groups = json.loads(file.read())
-        if chat_name in all_groups[chosen_group]['chats']:
-            context.bot.send_message(chat_id=update.effective_chat.id,
-                                     text='This chat already exists in this group')
-            return 3
-        all_groups[chosen_group]['chats'].append(chat_name)
-        with open('groups.json', 'w') as file:
-            file.write(json.dumps(all_groups))
-        context.bot.send_message(chat_id=update.effective_chat.id,
-                                 text='Done!')
-        AddChat.choosing_group_for_adding = ''
-        return 3
 
-    def ending_add_user(update: Update, context: CallbackContext):
-        username = update.message.text
-        chosen_group = AddChat.choosing_group_for_adding
-        with open('groups.json', 'r') as file:
-            all_groups = json.loads(file.read())
-        all_chats = all_groups[chosen_group]['chats']
-        del all_groups
-        if not all_chats:
-            context.bot.send_message(chat_id=update.effective_chat.id, text="This group doesn't have any chats")
-            return 3
-        for chat in all_chats:
-            result = add_user(chat, username)
-            context.bot.send_message(chat_id=update.effective_chat.id, text=result)
+def ending_add_chat(update: Update, context: CallbackContext):
+    chat_name = update.message.text
+    chosen_group = AddChat.choosing_group_for_adding
+    with open('groups.json', 'r') as file:
+        all_groups = json.loads(file.read())
+    if chat_name in all_groups[chosen_group]['chats']:
         context.bot.send_message(chat_id=update.effective_chat.id,
-                                 text='Done!')
-        AddChat.choosing_group_for_adding = ''
+                                 text='This chat already exists in this group')
         return 3
+    all_groups[chosen_group]['chats'].append(chat_name)
+    with open('groups.json', 'w') as file:
+        file.write(json.dumps(all_groups))
+    context.bot.send_message(chat_id=update.effective_chat.id,
+                             text='Done!')
+    AddChat.choosing_group_for_adding = ''
+    return 3
+
+
+def ending_add_user(update: Update, context: CallbackContext):
+    username = update.message.text
+    chosen_group = AddChat.choosing_group_for_adding
+    with open('groups.json', 'r') as file:
+        all_groups = json.loads(file.read())
+    all_chats = all_groups[chosen_group]['chats']
+    del all_groups
+    if not all_chats:
+        context.bot.send_message(chat_id=update.effective_chat.id, text="This group doesn't have any chats")
+        return 3
+    for chat in all_chats:
+        result = add_user(chat, username)
+        context.bot.send_message(chat_id=update.effective_chat.id, text=result)
+    context.bot.send_message(chat_id=update.effective_chat.id,
+                             text='Done!')
+    AddChat.choosing_group_for_adding = ''
+    return 3
+
 
 class DeleteChat:
+    all_groups = {}
     choice_group: str = ''
     choice: list = []
 
-    @run_async
-    def delete_chat(update: Update, context: CallbackContext):
-        with open('groups.json', 'r') as file:
-            all_groups = json.loads(file.read())
-        reply_markup = Markup(all_groups)
-        update.message.reply_text('Please choose a group:', reply_markup=reply_markup.return_keyboard())
-        return 1
 
-    @run_async
-    def delete_chat_button(update: Update, context: CallbackContext):
-        query = update.callback_query
-        data = query.data
-        choice = data.split('|||')[0]
-        if data == 'exit':
-            context.bot.delete_message(chat_id=query.message.chat_id,
-                                       message_id=query.message.message_id)
-            return 4
-        elif choice in ['prev', 'next']:
-            page = int(data.split('|||')[1])
-            with open('groups.json', 'r') as file:
-                all_groups = json.loads(file.read())
-            reply_markup = Markup(all_groups, page)
-            reply_markup.next() if choice == 'next' else reply_markup.prev()
-            context.bot.edit_message_text(text="Choose a group:",
-                                          chat_id=query.message.chat_id,
-                                          message_id=query.message.message_id,
-                                          reply_markup=reply_markup.return_keyboard())
-            return 1
-        DeleteChat.choice_group = choice
-        with open('groups.json', 'r') as file:
-            DeleteChat.choice = json.loads(file.read())[choice]['chats']
-        if not DeleteChat.choice:
-            context.bot.send_message(chat_id=query.message.chat_id,
-                                     text="This group doesn't have any chats")
-            return 1
-        reply_markup = Markup(DeleteChat.choice)
-        context.bot.edit_message_text(chat_id=query.message.chat_id,
+@run_async
+def delete_chat(update: Update, context: CallbackContext):
+    with open('groups.json', 'r') as file:
+        all_groups = json.loads(file.read())
+        DeleteChat.all_groups = all_groups
+    reply_markup = Markup(all_groups)
+    update.message.reply_text('Please choose a group:', reply_markup=reply_markup.return_keyboard())
+    return 1
+
+
+@run_async
+def delete_chat_button(update: Update, context: CallbackContext):
+    query = update.callback_query
+    data = query.data
+    choice = data.split('|||')[0]
+    if data == 'exit':
+        context.bot.delete_message(chat_id=query.message.chat_id,
+                                   message_id=query.message.message_id)
+        return 4
+    elif choice in ['prev', 'next']:
+        page = int(data.split('|||')[1])
+        reply_markup = Markup(DeleteChat.all_groups, page)
+        reply_markup.next() if choice == 'next' else reply_markup.prev()
+        context.bot.edit_message_text(text="Choose a group:",
+                                      chat_id=query.message.chat_id,
                                       message_id=query.message.message_id,
-                                      text='You choose {}\nChoose a chat:'.format(choice),
+                                      reply_markup=reply_markup.return_keyboard())
+        return 1
+    DeleteChat.choice_group = choice
+    DeleteChat.choice = DeleteChat.all_groups[choice]['chats']
+    if not DeleteChat.choice:
+        context.bot.send_message(chat_id=query.message.chat_id,
+                                 text="This group doesn't have any chats")
+        return 1
+    reply_markup = Markup(DeleteChat.choice)
+    context.bot.edit_message_text(chat_id=query.message.chat_id,
+                                  message_id=query.message.message_id,
+                                  text='You choose {}\nChoose a chat:'.format(choice),
+                                  reply_markup=reply_markup.return_keyboard())
+    return 2
+
+
+def ending_delete_chat(update: Update, context: CallbackContext):
+    query = update.callback_query
+    data = query.data
+    choice = data.split('|||')[0]
+    if data == 'exit':
+        context.bot.delete_message(chat_id=query.message.chat_id,
+                                   message_id=query.message.message_id)
+        return 3
+    elif choice in ['prev', 'next']:
+        page = int(data.split('|||')[1])
+        reply_markup = Markup(DeleteChat.choice, page)
+        reply_markup.next() if choice[0] == 'next' else reply_markup.prev()
+        context.bot.edit_message_text(text="Choose a chat:",
+                                      chat_id=query.message.chat_id,
+                                      message_id=query.message.message_id,
                                       reply_markup=reply_markup.return_keyboard())
         return 2
 
-    def ending_delete_chat(update: Update, context: CallbackContext):
-        query = update.callback_query
-        data = query.data
-        choice = data.split('|||')[0]
-        if data == 'exit':
-            context.bot.delete_message(chat_id=query.message.chat_id,
-                                       message_id=query.message.message_id)
-            return 3
-        elif choice in ['prev', 'next']:
-            page = int(data.split('|||')[1])
-            all_chats = DeleteChat.choice
-            reply_markup = Markup(all_chats, page)
-            reply_markup.next() if choice[0] == 'next' else reply_markup.prev()
-            context.bot.edit_message_text(text="Choose a chat:",
-                                          chat_id=query.message.chat_id,
-                                          message_id=query.message.message_id,
-                                          reply_markup=reply_markup.return_keyboard())
-            return 2
-        with open('groups.json', 'r') as file:
-            all_groups = json.loads(file.read())
-        all_groups[DeleteChat.choice_group]['chats'].remove(choice)
-        with open('groups.json', 'w') as file:
-            file.write(json.dumps(all_groups))
-        context.bot.edit_message_text(text='Done!',
-                                      chat_id=update.effective_chat.id,
-                                      message_id=query.message.message_id)
-        return 3
+    DeleteChat.all_groups[DeleteChat.choice_group]['chats'].remove(choice)
+    with open('groups.json', 'w') as file:
+        file.write(json.dumps(DeleteChat.all_groups))
+    DeleteChat.all_groups = {}
+    DeleteChat.choice_group = ''
+    DeleteChat.choice = []
+    context.bot.edit_message_text(text='Done!',
+                                  chat_id=update.effective_chat.id,
+                                  message_id=query.message.message_id)
+    return 3
 
 
 @run_async
